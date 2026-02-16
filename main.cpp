2ef488d90234f2c487b91c76fe9b44ff6ef04e9a
Aknez <sashamelnyk0410@gmail.com>
2026-02-16 10:19:32 +0100

    Initial Helium MVP

---
+++ main.cpp
@@ -0,0 +1,310 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <fstream>
+#include <unordered_set>
+#include <algorithm>
+
+// Create 2 types of nodes, an anchor is basically an email which an asset like a domain or an acc relies on 
+enum class NodeType{
+    Anchor, Asset
+};
+
+// Defining a node with an id, the type and what it is dependent on.
+struct Node{
+    std::string id;
+    NodeType type;
+    std::vector<std::string> dependencies;
+};
+
+// I'm tired of writing allat
+typedef std::unordered_map<std::string, Node> database_t; 
+
+// Basic functions to get all the nodes from the current loaded graph and add nodes to it
+void printNodes(const database_t& graph);
+void addNode(database_t& graph, int amountArgs, char* arguments[], NodeType typeOfNode);
+
+// Functions to return the database from the parsed file that will be later saved, basically a Saving system
+database_t loadGraph();
+void saveGraph(const database_t& graph);
+
+// Linking system to add dependencies.
+void linkNodes(database_t& graph, int argc, char* arguments[]);
+// Check if useless linking is done if A reaches B and B reaches C then A shouldn't reach C.
+bool isReachable(const database_t& graph, const std::string& startID, const std::string& targetID);
+bool dfs(const database_t& graph, std::unordered_set<std::string>& visited, const std::string& current, const std::string& target);
+
+void simulateNodeFailure(char* arguments[], const database_t& graph);
+bool failedDependenciesCheck(const Node& potentialFailedNode, const std::unordered_set<std::string>& failedNodes);
+
+int main(int argc, char* argv[]){
+    // Check if there's enough arguments otherwise throw error.
+    if (argc < 2 ) {
+        std::cout << "Helium -- Expected at least 1 argument!";
+        return 1;
+    }
+
+    // Load graph from file.
+    database_t retrievedGraph = loadGraph();
+    // Using argv 1 as a string for further string functions to later check with if statements what does the user want.
+    std::string argument1 = argv[1];
+
+    // Add either asset or anchor to make it more clearer for the user and then saving.
+    if (argument1 == "add-asset"){ addNode(retrievedGraph, argc, argv, NodeType::Asset); saveGraph(retrievedGraph);} 
+    else if (argument1 == "add-anchor"){ addNode(retrievedGraph, argc, argv, NodeType::Anchor); saveGraph(retrievedGraph);} 
+
+    // Print all nodes.
+    else if(argument1 == "list"){ printNodes(retrievedGraph); }
+    // Link a node to another node.
+    else if (argument1 == "link") {linkNodes(retrievedGraph, argc, argv); saveGraph(retrievedGraph);}
+
+    else if (argument1 == "simulate") {simulateNodeFailure(argv, retrievedGraph);}
+    // TBD: If unrecognized should throw a help guide/msg
+
+    return 0;
+}
+
+void printNodes(const database_t& graph){
+    // Loop through every pair in graph and print the string which is basically the name
+    for (auto const &i : graph){    
+        std::cout << i.first << std::endl; 
+    }
+}
+
+void addNode(database_t& graph, int amountArgs, char* arguments[], NodeType typeOfNode){
+    // Check if name is empty or else define new node
+    if (amountArgs < 3) {
+            std::cout << "Helium -- Empty name" << std::endl;
+    } else {
+        std::string argument2 = arguments[2];
+        auto itr = graph.find(argument2);
+
+        if (itr == graph.end()){
+            // Check if name already used/exists and create new node if not and add it to the unorganized map
+            Node newNode;
+            newNode.id = argument2;
+            newNode.type = typeOfNode;
+
+            graph.emplace(std::pair<std::string, Node> {argument2, newNode});
+            std::cout << "Added " << argument2 << " succesfully!" << std::endl;
+        } else {
+            std::cout << "Helium -- Name already in use!" << std::endl;
+        }
+    }
+}
+
+
+database_t loadGraph() {
+    // Get graph.db and create an empty graph to be returned with the parsed info.
+    std::ifstream GRAPH_FILE("graph.db");
+    database_t graph;
+
+    // Check if exists or opened
+    if (!GRAPH_FILE.is_open()) {
+        return graph;
+    }
+    
+    // Parse every line, honestly i have no clue how the for loop works but it does work.
+    for (std::string line; std::getline(GRAPH_FILE, line);){
+        if (line.empty())
+            continue;
+        
+        Node newNode;
+        NodeType type;
+        std::vector<std::string> dependencies;
+
+        // File is structured basically id|type|dependencies so extract until | and then delete it to repeat
+        std::string idString = line.substr(0, line.find('|'));
+        line.erase(0, line.find('|')+1);
+        std::string typeString = line.substr(0, line.find('|'));
+        line.erase(0, line.find('|')+1);
+        std::string dependenciesString = line;
+
+        // Assing basically type based on the typeText
+        if (typeString == "anchor"){
+            type = NodeType::Anchor;
+        } else {
+            type = NodeType::Asset;
+        }
+
+        // Make dependency by expecting a coma in the end of it if not then the coma will be npos.
+        while (!dependenciesString.empty()){
+            size_t pos = dependenciesString.find(',');
+
+            if (pos == std::string::npos){
+                dependencies.push_back(dependenciesString.substr(0, dependenciesString.length()));
+                break;
+            } else {
+                dependencies.push_back(dependenciesString.substr(0, pos));
+                dependenciesString.erase(0, pos+1);
+            }
+        } 
+        
+        // Replace all the values in the newnode and put it in the graph with the name (id in the node) and the node itself
+        newNode.id = idString;
+        newNode.type = type;
+        newNode.dependencies = dependencies;
+        graph.emplace(idString, newNode);
+    }  
+    return graph;
+}
+
+void saveGraph(const database_t& graph) {
+    std::ofstream GRAPH_FILE("graph.db");
+
+    // Safety measure
+    if (!GRAPH_FILE.is_open()){
+        std::cout << ("Helium -- Graph couldn't be opened") << std::endl;
+        return;
+    }
+
+    // Loop through every node in the graph to parse it on the file
+    for (auto const &i : graph) {
+        std::string type;
+        std::string dependencies;
+
+        if (i.second.type == NodeType::Anchor){
+            type = "anchor";
+        } else {
+            type = "asset";
+        }
+
+        // Parse dependencies with a coma as a separator, and just this basic first boolean to not put coma at front
+        bool first = true;
+        for (auto itr : i.second.dependencies) {
+            if (first){
+                dependencies.append(itr);
+                first = false;
+            } else {
+                dependencies.append(","+itr);
+            }
+        }
+
+        // Replace it all and put in a string to be saved and continue.
+        std::string savedData = i.second.id + "|" + type + "|" + dependencies;
+        GRAPH_FILE << savedData << "\n";
+    }
+}
+
+void linkNodes(database_t& graph, int argc, char* arguments[]){
+    if (argc < 4){
+        std::cout << "Helium -- Low amount of arguments!" << std::endl;
+        return;
+    }
+
+    std::string sourceID = arguments[2];
+    std::string targetID = arguments[3];
+
+    if (graph.find(sourceID) != graph.end() && graph.find(targetID) != graph.end()){
+
+        if (graph[sourceID].type == NodeType::Asset && graph[targetID].type == NodeType::Asset){
+            std::cout << "Invalid type of nodes, you cannot link an asset to an asset!" << std::endl;
+            return;
+        } else if (graph[sourceID].type == NodeType::Anchor && graph[targetID].type == NodeType::Asset){
+            std::cout << "Invalid type of nodes, you cannot link an anchor to an asset!" << std::endl;
+            return;
+        } else if (graph[sourceID].id == graph[targetID].id)
+        {
+            std::cout << "Invalid nodes, they are the same node!" << std::endl;
+            return;
+        } else if (isReachable(graph, targetID, sourceID))
+        {
+            std::cout << "Helium -- This link would create a circular dependency!" << std::endl;
+            return;
+        }
+        if(std::find(graph[sourceID].dependencies.begin(), graph[sourceID].dependencies.end(), graph[targetID].id) != graph[sourceID].dependencies.end()){
+            std::cout << "Helium -- Dependency already found in source!" << std::endl;
+            return;
+        } else{
+            graph[sourceID].dependencies.push_back(graph[targetID].id);
+            std::cout << "Added new link successfuly!";
+        }
+    } else {
+        std::cout << "Helium -- Couldn't find node/s, use list to see all the nodes." << std::endl;
+        return;
+    }
+}
+
+bool isReachable(const database_t& graph, const std::string& startID, const std::string& targetID) {
+    std::unordered_set<std::string> visited;
+    return dfs(graph, visited, startID, targetID);
+}
+
+bool dfs(const database_t& graph, std::unordered_set<std::string>& visited, const std::string& current, const std::string& target){
+    if (current == target)
+        return true;
+    
+    visited.insert(current);
+
+    for (const auto& itr: graph.at(current).dependencies){ 
+        if (visited.find(itr) == visited.end()){
+            if (dfs(graph, visited, itr, target))
+                return true;
+        }
+    }
+    
+    return false;
+}
+
+void simulateNodeFailure(char* arguments[], const database_t& graph){
+    std::string nodeName = arguments[2];
+
+    if (graph.find(nodeName) == graph.end()){
+        std::cout << "Couldn't find node!" << std::endl;
+        return;
+    } else {
+        std::unordered_set<std::string> failed;
+        failed.emplace(nodeName);
+
+        bool changed = true;
+        while (changed){
+            changed = false;
+            for (const auto& itr : graph){
+                if(failed.find(itr.first) == failed.end()){
+                    if (!itr.second.dependencies.empty() && failedDependenciesCheck(itr.second, failed)){
+                        failed.emplace(itr.first);
+                        changed = true;
+                    } 
+                } 
+            }
+        }
+        int failedAssets = 0;
+        int failedAnchors = 0;
+        
+        std::cout << '\n';
+        std::cout << "[Helium -- Simulation: " << nodeName << "]" << std::endl;
+        std::cout << "Failed Anchors:" << std::endl;
+        for (const auto &itr : failed){
+            if (graph.at(itr).type == NodeType::Anchor){
+                std::cout << "- " << itr << std::endl;
+                failedAnchors++;
+            }
+        }
+        
+
+        std::cout << '\n';
+        std::cout << "Failed Assets:" << std::endl;
+        for (const auto &itr : failed){
+            if (graph.at(itr).type == NodeType::Asset){
+                std::cout << "- " << itr << std::endl;
+                failedAssets++;
+            }
+        }
+        std::cout << '\n';
+        std::cout << "Total failed anchors: " << failedAnchors << std::endl;
+        std::cout << "Total failed assets: " << failedAssets << std::endl;
+        int totalFailed = failedAnchors + failedAssets;
+        std::cout << "Total failed:" << totalFailed << std::endl;
+    } 
+}
+
+bool failedDependenciesCheck(const Node& potentialFailedNode, const std::unordered_set<std::string>& failedNodes){
+    for (const std::string i : potentialFailedNode.dependencies){
+        if (failedNodes.find(i) == failedNodes.end()){
+            return false;
+        } 
+    }
+
+    return true;
+}
