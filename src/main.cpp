ef4aff437f309b170789a0d99be96e5ab935e6fa
Aknez <sashamelnyk0410@gmail.com>
2026-02-19 20:30:44 +0100

    Add Markdown export and finalize v0.2.0

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,8 @@ int main(int argc, char* argv[]){
     database_t retrievedGraph = loadGraph();
     // Using argv 1 as a string for further string functions to later check with if statements what does the user want.
     std::string argument1 = argv[1];
-
+    
+    // Make argument 1 lowercase for better UX
     std::transform(argument1.begin(), argument1.end(), argument1.begin(), [](unsigned char c){ return std::tolower(c); });
 
     // Add either asset or anchor to make it more clearer for the user and then saving.
@@ -28,12 +29,19 @@ int main(int argc, char* argv[]){
     // Link a node to another node.
     else if (argument1 == "link") {linkNodes(retrievedGraph, argc, argv); saveGraph(retrievedGraph);}
 
+    // Simulate what happens when a node fails, which returns all dependendencies that fall
     else if (argument1 == "simulate") {simulateNodeFailure(argv, retrievedGraph);}
 
+    // Return every anchor with its dependent nodes and percentage
     else if (argument1 == "spof") {spofReport(retrievedGraph);}
 
+    // Return amount of assets with a single dependency and anchors without any backup
     else if (argument1 == "audit") {auditReport(retrievedGraph);}
 
+    // Export to Markdown file as a report
+    else if (argument1 == "export") {exportMarkdownReport(retrievedGraph);}
+
+    // Return err
     else {std::cout << "Helium -- Command not found!" << std::endl;}
     return 0;
 }
